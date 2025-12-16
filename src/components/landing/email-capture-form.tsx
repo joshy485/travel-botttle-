359e56b3b280202eb81901b2c26d7bdd2b0fe5ca
Josh Latham <joshua.latham91@gmail.com>
2025-12-16 23:43:44 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/components/landing/email-capture-form.tsx
+++ src/components/landing/email-capture-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useFormState, useFormStatus } from "react-dom";
-import { useEffect } from "react";
+import { useActionState, useEffect } from "react";
+import { useFormStatus } from "react-dom";
 import { useToast } from "@/hooks/use-toast";
 import { subscribeToNewsletter } from "@/app/actions";
 import { Input } from "@/components/ui/input";
@@ -34,7 +34,7 @@ function SubmitButton() {
 }
 
 export function EmailCaptureForm() {
-  const [state, formAction] = useFormState(subscribeToNewsletter, initialState);
+  const [state, formAction] = useActionState(subscribeToNewsletter, initialState);
   const { toast } = useToast();
 
   useEffect(() => {
