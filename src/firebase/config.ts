c083b35a44b9d00e2f236bfce8253c1adcd6e04a
Josh Latham <joshua.latham91@gmail.com>
2025-12-17 00:47:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2809710753-e6d64",
+  "appId": "1:557821271308:web:970376b7a02c1a2d583d29",
+  "apiKey": "AIzaSyCPAarzS_8j6ZFT15Uq8J0y7grLtlf-1Xc",
+  "authDomain": "studio-2809710753-e6d64.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "557821271308"
+};
