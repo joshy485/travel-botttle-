ec76eaf991ab6e41e5d87d2f892f95358b4a36d7
Josh Latham <joshua.latham91@gmail.com>
2025-12-17 09:24:31 +0000

    Okay, please remove this image and send it back to how it was before bec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { SecurityFeature } from '@/components/landing/security-feature';
 import { HowItWorks } from '@/components/landing/how-it-works';
 import { ImageRail } from '@/components/landing/image-rail';
 import { Footer } from '@/components/landing/footer';
-import { FullWidthImage } from '@/components/landing/full-width-image';
 
 export default function Home() {
   return (
@@ -17,7 +16,6 @@ export default function Home() {
         <ProblemSolution />
         <Features />
         <SecurityFeature />
-        <FullWidthImage />
         <HowItWorks />
         <ImageRail />
       </main>
