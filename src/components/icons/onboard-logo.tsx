79e1d830fa7f8bdcb20ea358ae26e9f2c04c330c
Josh Latham <joshua.latham91@gmail.com>
2025-12-17 02:37:02 +0000

    Okay I have updated the logo. Please add this as the whole website's log

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -3,10 +3,10 @@ import { cn } from '@/lib/utils';
 
 export const OnboardLogo = ({ className }: { className?: string }) => (
   <Image
-    src="/images/Generated Image December 17, 2025 - 1_16PM.jpeg"
+    src="/images/Untitled Project (1).png"
     alt="ONBOARD Logo"
     width={120}
     height={32}
-    className={cn('object-contain mix-blend-screen', className)}
+    className={cn('object-contain', className)}
   />
 );
