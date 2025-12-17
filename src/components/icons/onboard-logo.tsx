6d7b4a602c3ef207cc0adc09e19d1595432c2037
Josh Latham <joshua.latham91@gmail.com>
2025-12-17 02:21:51 +0000

    Please update the whole logo on the whole website with my logo. This is

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export const OnboardLogo = ({ className }: { className?: string }) => (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn('h-8 w-8 text-primary', className)}>
-        <path d="M16 3.5L28.5 10V22L16 28.5L3.5 22V10L16 3.5Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M10 13L16 16.5L22 13" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
+  <Image
+    src="/images/Untitled Project.png"
+    alt="ONBOARD Logo"
+    width={32}
+    height={32}
+    className={cn('h-8 w-8', className)}
+  />
 );
