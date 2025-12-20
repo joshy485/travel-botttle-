40258ddc4cc15d31c57813d8d4c9a6d5b8df50c9
Josh Latham <joshua.latham91@gmail.com>
2025-12-20 01:50:15 +0000

    Still nothing. Why is this

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -1,9 +1,10 @@
+
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export const OnboardLogo = ({ className }: { className?: string }) => (
   <Image
-    src="/images/Untitled%20Project%20(1).png"
+    src="/images/onboard-logo.png"
     alt="ONBOARD Logo"
     width={180}
     height={48}
