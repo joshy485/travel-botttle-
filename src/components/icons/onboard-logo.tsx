5f908e2a5eeac8112cb4ce04f1692abdf7ead422
Josh Latham <joshua.latham91@gmail.com>
2025-12-30 08:49:20 +0000

    Okay, some of the images are not loading, and the logo has stopped worki

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 
 export const OnboardLogo = ({ className }: { className?: string }) => (
   <Image
-    src="/images/onboard-logo.png"
+    src="/images/Whisk_935a5efb63f575bb9c940da82f6c9764dr.png"
     alt="ONBOARD Logo"
     width={180}
     height={48}
