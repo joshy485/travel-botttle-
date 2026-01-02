86f2105d324da6c70474cdb0f219cd9260a75a84
Josh Latham <joshua.latham91@gmail.com>
2026-01-02 00:09:44 +0000

    Okay, I have found the link. What the bucket is called, this is it.  /we

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function OnboardLogo() {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/studio-2809710753-e6d64.appspot.com/o/website%20photos%2Flogo.png?alt=media"
+      src="https://firebasestorage.googleapis.com/v0/b/studio-2809710753-e6d64.appspot.com/o/website%20photos%2FoGveJGIx6uAr2SSz6ToC%2Flogo.png?alt=media"
       alt="ONBOARD"
       width={140}
       height={32}
