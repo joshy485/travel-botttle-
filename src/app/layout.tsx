ea0eff66573c828a609a82c3d9df79ca81a3fbd4
Joshua <josh@onboard-store.com>
2026-01-02 13:47:09 +1100

    Fix broken images and logo, update assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className={cn("font-body antialiased", "min-h-screen bg-background font-sans", inter.variable)}>
         <FirebaseClientProvider>
           {children}
--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function OnboardLogo() {
   return (
     <Image
-      src="https://firebasestorage.googleapis.com/v0/b/studio-2809710753-e6d64.appspot.com/o/logo.png?alt=media"
+      src="/images/logo.png"
       alt="ONBOARD"
       width={140}
       height={32}
