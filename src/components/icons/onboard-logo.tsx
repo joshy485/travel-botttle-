0e6b1566276d26219bc5573e1c0d3af57d0e6c53
Josh Latham <joshua.latham91@gmail.com>
2026-01-02 00:06:54 +0000

    Okay, in the Firestore database, I have added a new folder called "websi

--- src/components/icons/onboard-logo.tsx
+++ src/components/icons/onboard-logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function OnboardLogo() {
   return (
     <Image
-      src="/images/logo.png"
+      src="https://firebasestorage.googleapis.com/v0/b/studio-2809710753-e6d64.appspot.com/o/website%20photos%2Flogo.png?alt=media"
       alt="ONBOARD"
       width={140}
       height={32}
